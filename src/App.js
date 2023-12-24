ff32b2b6030c18cc9e94215933b96a151cf8b4ef
Christina Li <christinayxli@gmail.com>
2023-12-24 18:22:49 -0500

    refactored some code

--- src/App.js
+++ src/App.js
@@ -1,44 +1,31 @@
-import scrabbleLetter from "./scrabble-letter.png";
-import "./App.css";
+import scrabbleLetter from "./components/scrabble-letter.png";
+import "./components/App.css";
+import Homepage from "./components/homepage";
 import Modal from "./components/modal";
 import Navbar from "./components/navbar";
 import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+
+function PlayNow() {
+  return (
+      <h2>Play Now Page</h2>
+  );  
+}
+function Home() {
+  return (
+    <Homepage></Homepage>
+  );
+}
+
 
 function App() {
-  const [helpOpen, setHelpOpen] = useState(false);
   return (
-    <div className="App">
-      <div className="main">
-       <Navbar></Navbar>
-      <div className="app-container">
-        <div className="flex-none">
-          <div className="flex flex-row justify-center">
-            <img className="m-4 logo" src={scrabbleLetter}></img>
-          </div>
-          <h1 className="text-5xl font-bold"> Word<mark>bits</mark> </h1>
-          <p className="app-subtitle p-2 text-1xl">
-            find hidden words, guess their meaning to win
-          </p>
-          <div className="flex flex-row justify-center">
-            <button
-              onClick={() => setHelpOpen(true)}
-              className="rounded-lg font-medium border-solid border-2 border-green-800 hover:bg-green-900 hover:text-white px-4 py-1 m-4"
-            >
-              How to play
-            </button>
-            <button className="rounded-lg font-medium border-solid border-2 border-green-800 hover:bg-green-900 hover:text-white px-4 py-1 m-4">
-              Login
-            </button>
-            <button className="rounded-lg font-medium border-solid border-2 border-green-800 hover:bg-green-900  hover:text-white px-4 py-1 m-4">
-              Play now
-            </button>
-          </div>
-          <p className="p-2 text-sm"> Last updated: Dec 22nd 2023</p>
-        </div>
-      </div>
-      {helpOpen && <Modal onClose={() => setHelpOpen(false)}></Modal>}
-    </div>
-    </div>
+    <Router>
+        <Routes>
+          <Route path="/play-now" element={<PlayNow/>} />
+          <Route path="/" element={<Home/>} />
+        </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/homepage.jsx
@@ -0,0 +1,56 @@
+import Navbar from "./navbar";
+import Modal from "./modal"
+import scrabbleLetter from "./scrabble-letter.png";
+import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+export default function Homepage() {
+    const [helpOpen, setHelpOpen] = useState(false);
+    const [playNowClicked, setPlayNowClicked] = useState(false);
+return (
+    <div className="App">
+      <div className="main">
+        <Navbar></Navbar>
+        <div className="app-container">
+          <div className="flex-none">
+            <div className="flex flex-row justify-center">
+              <img className="m-4 logo" src={scrabbleLetter}></img>
+            </div>
+            <h1 className="text-5xl font-bold">
+              {" "}
+              Word<mark>bits</mark>{" "}
+            </h1>
+            <p className="app-subtitle p-2">
+              find hidden words, guess their meaning to win
+            </p>
+            <div className="flex flex-row justify-center">
+              <button
+                onClick={() => {
+                  setHelpOpen(true);
+                }}
+                className="rounded-lg border-solid border-2 border-green-800 hover:bg-green-900 hover:text-white py-2 px-5 m-4"
+              >
+                How to play
+              </button>
+              <button className="rounded-lg font-medium border-solid border-2 border-green-800 hover:bg-green-900 hover:text-white  py-2 px-5 m-4">
+                Login
+              </button>
+              <Link to="/play-now">
+                <button
+                  onClick={() => {
+                    setPlayNowClicked(true);
+                  }}
+                  className="rounded-lg font-medium border-solid border-2 border-green-800 hover:bg-green-900  hover:text-white   py-2 px-5 m-4"
+                >
+                  Play now
+                </button>
+              </Link>
+            </div>
+            <p className="p-2 text-sm"> Last updated: Dec 22nd 2023</p>
+          </div>
+        </div>
+        {helpOpen && <Modal onClose={() => setHelpOpen(false)}></Modal>}
+      </div>
+    </div>
+  )
+}
