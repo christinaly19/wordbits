0f6c2481e15032b6b438155cec79fc46657d7125
Christina Li <christinayxli@gmail.com>
2023-12-25 16:00:34 -0500

    finished selection mechanism

--- src/components/gameboard.jsx
+++ src/components/gameboard.jsx
@@ -4,23 +4,61 @@ import React, { useState, memo, useEffect } from "react";
 import WordDisplay from "./wordDisplay";
 const GameBoard = (letterArray) => {
   const [currWord, setCurrWord] = useState([]);
+  const [coordinates, setCoordinates] = useState([]);
+  const [gameStarted, setGameStarted] = useState(false);
 
+  useEffect(() => {
+    if (coordinates.length === 0) {
+      setGameStarted(false);
+    } else {
+      setGameStarted(true);
+    }
+  }, [coordinates]);
+
+  const isAdjacent = (x1, y1, x2, y2) => {
+    return (
+      Math.abs(x1 - x2) <= 1 &&
+      Math.abs(y1 - y2) <= 1 &&
+      !(x1 === x2 && y1 === y2)
+    );
+  };
+  useEffect(() => {
+    console.log(coordinates);
+  }, [coordinates]);
   const renderGrids = () => {
     const twoDArray = letterArray.letterArray;
     return twoDArray.map((row, rowIndex) => (
       <div key={rowIndex} className="grid-row">
         {Array.isArray(row)
-          ? row.map((letter, colIndex) => (
-              <div
-                key={colIndex}
-                onClick={() =>
-                  setCurrWord((prevCurrWord) => [...prevCurrWord, letter])
-                }
-                className="gameboard-grid-item hover:bg-emerald-600"
-              >
-                {letter}
-              </div>
-            ))
+          ? row.map((letter, colIndex) => {
+              const isHovered = coordinates.some((coord) =>
+                isAdjacent(coord.x, coord.y, rowIndex, colIndex)
+              );
+
+              return (
+                <div
+                  key={colIndex}
+                  onClick={() => {
+                    setCurrWord((prevCurrWord) => [...prevCurrWord, letter]);
+                    setCoordinates((prevCoordinates) => [
+                      ...prevCoordinates,
+                      { x: rowIndex, y: colIndex },
+                    ]);
+                  }}
+                  className={`gameboard-grid-item ${
+                    isHovered ? "hover:bg-emerald-600" : ""
+                  } ${
+                    coordinates.some(
+                      (coord) => coord.x === rowIndex && coord.y === colIndex
+                    )
+                      ? "selected"
+                      : ""
+                  }`}
+                >
+                  {letter}
+                </div>
+              );
+            })
           : null}
       </div>
     ));
@@ -29,15 +67,19 @@ const GameBoard = (letterArray) => {
     <>
       <div className="gameboard-main">
         <Navbar></Navbar>
-        <div className="gameboard-grid-and-box flex">
+        {!gameStarted ?
+        <div className="italic text-center instructions mt-5"> Click on any letter to start</div> :
+        <div className="italic text-center instructions mt-5"> Press to enter or clear</div> 
+        }
+        <div className="gameboard-grid-and-box mt-5 flex">
           <div className="ml-20 gameboard-grid grid grid-rows-24 grid-flow-col">
             {renderGrids()}
           </div>
           <div>
-            <div className="ml-10 py-1 px-3 gameboard-currword-box flex justify-center mt-5 text-2xl font-semibold">
+            <div className="ml-10 py-1 px-3 gameboard-currword-box flex justify-center text-2xl font-semibold">
               {currWord}
             </div>
-            <div className="flex justify-center ml-10 py-1 px-3 gameboard-word-box">
+            <div className="mt-10 flex justify-center ml-10 py-1 px-3 gameboard-word-box">
               <div className="flex justify-center"></div>
               <h1 className="font-semibold"> Words Found (0) </h1>
             </div>
