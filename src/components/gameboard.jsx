f81496a2e4cb26acd599fd13199b4e5d2a8d42a4
Christina Li <christinayxli@gmail.com>
2023-12-25 13:01:48 -0500

    changed layout a little

--- src/components/gameboard.jsx
+++ src/components/gameboard.jsx
@@ -3,18 +3,21 @@ import Navbar from "./navbar";
 import React, { useState, memo, useEffect } from "react";
 import WordDisplay from "./wordDisplay";
 const GameBoard = (letterArray) => {
-const [currWord, setCurrWord] = useState([]);
+  const [currWord, setCurrWord] = useState([]);
 
   const renderGrids = () => {
-    const twoDArray = letterArray.letterArray
+    const twoDArray = letterArray.letterArray;
     return twoDArray.map((row, rowIndex) => (
       <div key={rowIndex} className="grid-row">
         {Array.isArray(row)
           ? row.map((letter, colIndex) => (
               <div
                 key={colIndex}
-                onClick={() => setCurrWord((prevCurrWord) => [...prevCurrWord, letter])}
-                className="gameboard-grid-item hover:bg-emerald-600">
+                onClick={() =>
+                  setCurrWord((prevCurrWord) => [...prevCurrWord, letter])
+                }
+                className="gameboard-grid-item hover:bg-emerald-600"
+              >
                 {letter}
               </div>
             ))
@@ -30,12 +33,15 @@ const [currWord, setCurrWord] = useState([]);
           <div className="ml-20 gameboard-grid grid grid-rows-24 grid-flow-col">
             {renderGrids()}
           </div>
-          <div className="flex justify-center ml-14 px-4 gameboard-word-box">
-            <h1 className="font-semibold"> Found Words </h1>
+          <div>
+            <div className="ml-10 py-1 px-3 gameboard-currword-box flex justify-center mt-5 text-2xl font-semibold">
+              {currWord}
+            </div>
+            <div className="flex justify-center ml-10 py-1 px-3 gameboard-word-box">
+              <div className="flex justify-center"></div>
+              <h1 className="font-semibold"> Words Found (0) </h1>
+            </div>
           </div>
-        </div>
-        <div className="flex justify-center">
-        <div className="gameboard-currword-box flex justify-center mt-5 text-2xl font-semibold"> {currWord} </div>
         </div>
       </div>
     </>
